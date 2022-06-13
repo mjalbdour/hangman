919b20b6a34005c0c0a77b60673f40df3a8250bd
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-14 02:18:10 +0300

    Stage 2/8: Let’s play a game

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,3 +1,14 @@
 # Write your code here
 print('H A N G M A N')
-print('The game will be available soon.')
+
+answer = 'python'
+msg_win = 'You survived!'
+msg_lost = 'You lost!'
+
+print('Guess the word: > ')
+guess = input()
+
+if guess == answer:
+    print(msg_win)
+else:
+    print(msg_lost)
